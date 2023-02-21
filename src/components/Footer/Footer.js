c5013b82d11a877ceb063523736732045ec6783d
maxt0d0nt <maxbox21@hotmail.com>
2023-02-21 20:38:20 +0100

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import logo from '../../assets/img/logoFooter.png'
+import logo from '../../assets/img/Logo.png'
 import { FaFacebook, FaWhatsapp, FaInstagram, FaTwitter } from "react-icons/fa";
 import { IconContext } from 'react-icons';
 
@@ -9,38 +9,38 @@ const Footer = () => {
         <>
             <div className='footer-container'>
                 <div className='section-footer-1'>
-                <div className='logo-footer'>
-                    <img src={logo} alt="logo-img" />
-                    <p>CONSULTORIO YOLANDA DELGADO</p>
-                </div>
-                <div className='rrss-link-footers'>
-                <IconContext.Provider value={{ size: "3em", color: "#fff", }}>
-                    <FaFacebook />
-                    <FaInstagram />
-                    <FaTwitter />
-                    <FaWhatsapp />
-                    </IconContext.Provider>
-                </div>
+                    <div className='logo-footer'>
+                        <img src={logo} alt="logo-img" className='logo-img'/>
+                        <p>CONSULTORIO YOLANDA DELGADO</p>
+                    </div>
+                    <div className='rrss-link-footers'>
+                        <IconContext.Provider value={{ size: "3em", color: "#fff", }}>
+                            <FaFacebook />
+                            <FaInstagram />
+                            <FaTwitter />
+                            <FaWhatsapp />
+                        </IconContext.Provider>
+                    </div>
                 </div>
                 <div className='sector-footer-2'>
-                <div className='menu-footer'>
-                    <div className='link-footer'>
-                        ¿PORQUE ELEGIRNOS?
+                    <div className='menu-footer'>
+                        <div className='link-footer'>
+                            ¿PORQUE ELEGIRNOS?
+                        </div>
+                        <div className='link-footer'>
+                            ESPECIALIDADES
+                        </div>
+                        <div className='link-footer'>
+                            PERSONAL
+                        </div>
                     </div>
-                    <div className='link-footer'>
-                        ESPECIALIDADES
+                    <div className='address-footer'>
+                        <p> DIRECCION:</p>
+                        <p>AGUA SALUD - CARACAS</p>
+                        <p>HORARIOS:</p>
+                        <p>LUN a VIE</p>
+                        <p>DE 8:00 AM a 4:00 PM</p>
                     </div>
-                    <div className='link-footer'>
-                        PERSONAL
-                    </div>
-                </div>
-                <div className='address-footer'>
-                    <p> DIRECCION:</p>
-                    <p>AGUA SALUD - CARACAS</p>
-                    <p>HORARIOS:</p>
-                    <p>LUN a VIE</p>
-                    <p>DE 8:00 AM a 4:00 PM</p>
-                </div>
                 </div>
             </div>
         </>
